c6b5c9c22d558ab8333d8183bb20ab8ff1df09d3
aimansae <saeedaimann@gmail.com>
2022-09-26 18:11:22 +0000

    Connect email.js

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,16 @@
-console.log('helo')
+const { EmailJSResponseStatus } = require("@emailjs/browser");
+
+function sendMail(contactForm){
+    emailjs.send('gmail', 'rosie', {
+        //nams from the form
+        'from_name': contactForm.name.value,
+        'from_name': contactForm.name.value,
+        'project_request': contactForm.projectsummary.value,
+    } )
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+     }, function(error) {
+        console.log('FAILED...', error);
+     });
+     return false;  // To block from loading a new page
+    }
